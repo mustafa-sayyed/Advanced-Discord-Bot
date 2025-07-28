94811983df8acc739b192602c6ba96d20dc943fb
DeadIndian <gollabharath2007@gmail.com>
2025-07-28 19:46:29 +0530

    Added /shop

--- utils/database.js
+++ utils/database.js
@@ -10,6 +10,7 @@ const {
   TruthOrDareConfig,
   AntiRaid,
   GuildEconomy,
+  ShopItem,
 } = require("../models/schemas");
 
 class Database {
@@ -30,6 +31,7 @@ class Database {
     this.AntiRaid = AntiRaid;
     this.GuildConfig = ServerConfig; // Alias for ServerConfig
     this.GuildEconomy = GuildEconomy;
+    this.ShopItem = ShopItem;
   }
 
   static async getInstance() {
